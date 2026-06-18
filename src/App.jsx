faba727bff9ff10463e46143b644ff5f736c9fee
Zsolt Alapi <zsalapi@gmail.com>
2026-06-18 12:08:48 +0200

    Checkbox és Regisztráció gomb

--- src/App.jsx
+++ src/App.jsx
@@ -10,42 +10,122 @@ function App() {
         <div className="col-md-6 d-flex align-items-center justify-content-center p-5">
           <div className="w-100" style={{ maxWidth: "80%" }}>
             <h1>Regisztráció</h1>
-             <label className="text-center">Add meg e-mail címedet és jelszavadat a regisztrációhoz!</label>
-            <div><br></br></div>
+            <label className="text-center">
+              Add meg e-mail címedet és jelszavadat a regisztrációhoz!
+            </label>
+            <div>
+              <br></br>
+            </div>
             <form>
               <div className="row">
                 <div className="col-md-6 mb-3">
-                  <label className="form-label d-block text-start">Vezetéknév <span style={{ color: 'red', fontSize: '0.8em', verticalAlign: 'super' }}>*</span></label>
-                  <input type="text" className="form-control" placeholder="Teszt" required />
+                  <label className="form-label d-block text-start">
+                    Vezetéknév{" "}
+                    <span
+                      style={{
+                        color: "red",
+                        fontSize: "0.8em",
+                        verticalAlign: "super",
+                      }}
+                    >
+                      *
+                    </span>
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Teszt"
+                    required
+                  />
                 </div>
                 <div className="col-md-6 mb-3">
-                  <label className="form-label d-block text-start">Keresztnév <span style={{ color: 'red', fontSize: '0.8em', verticalAlign: 'super' }}>*</span></label>
-                  <input type="text" className="form-control" placeholder="Elek" required />
+                  <label className="form-label d-block text-start">
+                    Keresztnév{" "}
+                    <span
+                      style={{
+                        color: "red",
+                        fontSize: "0.8em",
+                        verticalAlign: "super",
+                      }}
+                    >
+                      *
+                    </span>
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Elek"
+                    required
+                  />
                 </div>
               </div>
               <div className="mb-3">
-                <label className="form-label d-block text-start">Email cím <span style={{ color: 'red', fontSize: '0.8em', verticalAlign: 'super' }}>*</span></label>
-                <input type="email" className="form-control" placeholder="example@mail.com" required />
+                <label className="form-label d-block text-start">
+                  Email cím{" "}
+                  <span
+                    style={{
+                      color: "red",
+                      fontSize: "0.8em",
+                      verticalAlign: "super",
+                    }}
+                  >
+                    *
+                  </span>
+                </label>
+                <input
+                  type="email"
+                  className="form-control"
+                  placeholder="example@mail.com"
+                  required
+                />
               </div>
               <div className="mb-3">
-                <label className="form-label d-block text-start">Jelszó <span style={{ color: 'red', fontSize: '0.8em', verticalAlign: 'super' }}>*</span></label>
-                <input type="password" className="form-control" placeholder="************" required />
+                <label className="form-label d-block text-start">
+                  Jelszó{" "}
+                  <span
+                    style={{
+                      color: "red",
+                      fontSize: "0.8em",
+                      verticalAlign: "super",
+                    }}
+                  >
+                    *
+                  </span>
+                </label>
+                <input
+                  type="password"
+                  className="form-control"
+                  placeholder="************"
+                  required
+                />
               </div>
               <div className="mb-3 form-check d-block text-start">
-                <input
-                  type="checkbox"
-                  className="form-check-input"
-                  id="check"
-                />
-                <label className="form-check-label d-block text-start" htmlFor="check">A fiók létrehozásával elfogadja a <a href="#">Felhasználási feltételeket</a> és az <a href="#">Adatvédelmi irányelveket</a>
-                </label>
-              </div>
-              <button type="submit" className="btn btn-primary w-100 mb-3">
+              
+                  <div className="mb-3 d-flex align-items-center">
+                    <input
+                      type="checkbox"
+                      className="form-check-input mt-0"
+                      id="check"
+                    />&nbsp;
+                    <label
+                      className="form-check-label ms-2 text-start"
+                      htmlFor="check"
+                      style={{ color: "#667085", fontSize: "1rem" }}
+                    >
+                      Fiók létrehozásával elfogadja a{" "}
+                      <a href="#">Felhasználási feltételeket,</a>
+                      <br />
+                      és az <a href="#">Adatvédelmi irányelveket</a>
+                    </label>
+                  </div>
+                </div>
+              <button type="submit" className="btn btn-primary h-15 w-100 mb-3">
                 Regisztráció
               </button>
             </form>
 
-            <p className="text-center">Már van fiókja?</p><a href="#">Bejelentkezés</a>
+            <p className="text-center">Már van fiókja?</p>
+            <a href="#">Bejelentkezés</a>
 
             <div className="d-flex align-items-center my-3">
               <hr className="flex-grow-1" />
